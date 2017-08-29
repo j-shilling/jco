53f9dcff712ffa87370502c66861c03d3234537e
Jake Shilling <shilling.jake@gmail.com>
2017-08-29 07:44:30 -0400

    Removing functions from mutable collection which only make sense for lists.

--- include/jco/api/mutablecollection.h
+++ include/jco/api/mutablecollection.h
@@ -15,8 +15,5 @@ void mutable_collection_clear (const void *_self);
 bool mutable_collection_remove (const void *_self, const void *o);
 bool mutable_collection_remove_all (const void *_self, const void *c);
 bool mutable_collection_retain_all (const void *_self, const void *c);
-void *mutable_collection_set (const void *_self, unsigned int index, const void *o);
-void mutable_collection_sort (const void *_self, Comparator comparator);
-void *mutable_collection_sublist (const void *_self, unsigned int start, unsigned int end);
 
 #endif /* INCLUDE_OBJECT_API_MUTABLECOLLECTION_H_ */
